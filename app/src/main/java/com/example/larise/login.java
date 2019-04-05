f1b7cfab9f2f19b2a37d65d5752051dd10c9dc57
rhmdnrhuda <rhmdnrhuda@gmail.com>
2019-04-05 13:18:13 +0700

    Login Dengan Firebase

--- app/src/main/java/com/example/larise/login.java
+++ app/src/main/java/com/example/larise/login.java
@@ -1,13 +1,64 @@
 package com.example.larise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email);
+        password  = findViewById(R.id.password);
+        login   = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().isEmpty()||password.getText().toString().isEmpty()){
+                    Toast.makeText(login.this, "Email atau password tidak boleh kosong",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    signIn();
+                }
+            }
+        });
+    }
+
+    public void signIn(){
+        String email = this.email.getText().toString();
+        String password = this.password.getText().toString();
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(login.this, "Authentication Succeded.",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
